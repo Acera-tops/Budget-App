6c6eda79550530da44071695f04f225fdd405a33
april <apriltopolewski@gmail.com>
2020-12-10 17:32:04 -0500

    Cleaned up and added comments for clarity

--- script.js
+++ script.js
@@ -13,47 +13,38 @@ let transactionHistory = [
    }, 
 ];
 
-
-//Source: https://www.valentinog.com/blog/html-table/
-
- function generateTableHead(table, data) {
-   let thead = table.createTHead();
-   let row = thead.insertRow();
-   for (let key of data) {
-     let th = document.createElement("th");
-     let text = document.createTextNode(key);
-     th.appendChild(text);
-     row.appendChild(th);
+//Generating Transaction History in HTML table from transactionHistory Array
+   function generateTableHead(table, data) {
+      let thead = table.createTHead();
+      let row = thead.insertRow();
+      for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+      }
    }
- }
 
- function generateTable(table, data) {
-   for (let element of data) {
-     let row = table.insertRow();
-     for (key in element) {
-       let cell = row.insertCell();
-       let text = document.createTextNode(element[key]);
-       cell.appendChild(text);
-     }
+   function generateTable(table, data) {
+      for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+         let cell = row.insertCell();
+         let text = document.createTextNode(element[key]);
+         cell.appendChild(text);
+      }
+      }
    }
- }
-  
- let table = document.querySelector("table");
- let data = Object.keys(transactionHistory[0]);
- generateTable(table, transactionHistory);
- generateTableHead(table, data);
+   
+   let table = document.querySelector("table");
+   let data = Object.keys(transactionHistory[0]);
+   generateTable(table, transactionHistory);
+   generateTableHead(table, data);
 
-
- 
-//  console.table(values) - Using an array to tabulate a set of values
-
-// - onClickSubmit - listener for event on new input
-// - transactionHistory.unshift(newPurchase) - add input as new row in array
-// - insertRow(transactionHistory index 0) - Creates create (tr) in html with inputted data
-//    const newInput = arr[Symbol.iterator]();
-//    const newRow = document.getElementById(transactionHistory);
-//    for (let data of newInput) {
-//    transactionHistory.innerHTML += "<tr>" + "<td>"+ date + "</td>" + "<td>" + description + "</td>" + "<td>" + category + "</td>" + "<td>" + amount + "</td>" + "</tr>";
-//    }
 
 // - array.prototype.filter() - Returns a new array containing all elements of the calling array for which the provided filtering function returns true.
+
+
+
+
+//Sources: https://www.valentinog.com/blog/html-table/
