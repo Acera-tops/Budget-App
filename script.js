0ef319de116c583be2396b5acae8dd977255d06d
april <apriltopolewski@gmail.com>
2020-12-14 18:46:20 -0500

    updated transaction history array

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let transactionHistory = [
    {
-    Date: "12/15/2020",
-    Description: "DTE Energy",
-    Category: "Bills",
-    Amount: "$68.50",
-   }, 
-   {
-    Date: "12/01/2020",
-    Description: "Movies",
-    Category: "Entertainment",
-    Amount: "$7.99",
+    Date: "yyyy-mm-dd",
+    Description: "What you purchased",
+    Category: "Bills, Food, Entertainment, Clothing ",
+    Amount: "Amount Spent",
    }, 
 ];
 
